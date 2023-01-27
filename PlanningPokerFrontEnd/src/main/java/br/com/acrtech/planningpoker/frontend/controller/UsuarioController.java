398180d6cf0e931990dca59ca8d2bc1412bc4322
acrsantana <cezaodabahia@gmail.com>
2023-01-27 17:37:39 -0300

    Front End - páginas estáticas

--- PlanningPokerFrontEnd/src/main/java/br/com/acrtech/planningpoker/frontend/controller/UsuarioController.java
+++ PlanningPokerFrontEnd/src/main/java/br/com/acrtech/planningpoker/frontend/controller/UsuarioController.java
@@ -1,12 +1,17 @@
 package br.com.acrtech.planningpoker.frontend.controller;
 
+import br.com.acrtech.planningpoker.frontend.dto.UsuarioDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
-@RequestMapping("/usuarios")
 public class UsuarioController {
+
+    @GetMapping("/")
+    public String welcome(){
+        return "welcome";
+    }
     @GetMapping("/cadastro")
     public String cadastro() {
         return "cadastroUsuario";
@@ -15,5 +20,17 @@ public class UsuarioController {
     @GetMapping("/login")
     public String login() {
         return "login";
+    }
+
+    @PostMapping("/login")
+    public String autenticar(UsuarioDto usuarioDto) {
+        System.out.println(usuarioDto);
+        return "redirect:/";
+    }
+
+    @PostMapping("/cadastro")
+    public String cadastrar(UsuarioDto usuarioDto) {
+        System.out.println(usuarioDto);
+        return "redirect:/";
     }
 }
